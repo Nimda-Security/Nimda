3f8291f57c30b0a50830e1397949446f90e6d32b
unknown <xtkww97178@gmail.com>
2026-03-19 17:01:20 +0900

    fix: 엔티티-DTO 필드 불일치 및 컴파일 에러 수정

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/point/entity/PointDetail.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/point/entity/PointDetail.java
@@ -1,10 +1,7 @@
 package com.nimda.cite.point.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
