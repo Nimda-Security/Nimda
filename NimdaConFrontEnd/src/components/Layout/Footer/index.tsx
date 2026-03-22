f65a773ca96c2a6a80175f1b8f331c52ba27bc77
nuyoes <s471y@kakao.com>
2026-03-22 22:38:12 +0900

    feat. footer 비즈니스 문의 복붙 기능 추가

--- NimdaConFrontEnd/src/components/Layout/Footer/index.tsx
+++ NimdaConFrontEnd/src/components/Layout/Footer/index.tsx
@@ -2,6 +2,14 @@ import React from "react";
 import Logo from "@/components/icons/Logo";
 
 const Footer: React.FC = () => {
+  const [showToast, setShowToast] = React.useState(false);
+
+  const handleCopyEmail = () => {
+    navigator.clipboard.writeText("nimda0410@gmail.com");
+    setShowToast(true);
+    setTimeout(() => setShowToast(false), 2000);
+  };
+
   return (
     <footer className="layout__footer">
       <div className="layout__footer-inner">
@@ -37,7 +45,12 @@ const Footer: React.FC = () => {
             <span>개인정보 보호정책</span>
             <span>청소년 보호정책</span>
             <span>사이트 이용규칙</span>
-            <span>비즈니스 문의</span>
+            <span 
+              onClick={handleCopyEmail} 
+              style={{ cursor: "pointer" }}
+            >
+              비즈니스 문의
+            </span>
           </div>
 
           {/* 저작권 */}
@@ -46,6 +59,13 @@ const Footer: React.FC = () => {
           </p>
         </div>
       </div>
+
+      {/* 플로팅 토스트 팝업 */}
+      {showToast && (
+        <div className="copy-toast">
+          이메일 주소가 복사되었습니다.
+        </div>
+      )}
     </footer>
   );
 };
