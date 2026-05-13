6d01712c4245de663a87b42995a193134390c9a4
unknown <xtkww97178@gmail.com>
2026-05-13 15:49:31 +0900

    feat: SesConfig update
    메일 발송에 필요한 기본 설정 추가 및 pom 업데이트

---
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/config/SesConfig.java
@@ -0,0 +1,42 @@
+package com.nimda.cite.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class SesConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true"); // SES는 TLS 필수
+        props.put("mail.debug", "true"); // 로그에서 발송 과정을 보고 싶을 때
+
+        return mailSender;
+    }
+}
