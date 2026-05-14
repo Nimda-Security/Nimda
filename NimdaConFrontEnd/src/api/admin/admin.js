e2aa5dcd12d4fe9719ac2b41823236c46f48af17
이명건 <audrjs987@naver.com>
2026-05-14 12:14:10 +0900

    fix: 어드민 페이지 사용자 정보 안보이는 문제

--- NimdaConFrontEnd/src/api/admin/admin.js
+++ NimdaConFrontEnd/src/api/admin/admin.js
@@ -62,7 +62,7 @@ export const getAdminUserDetailAPI = async (userId) => {
     const result = await parseJsonSafe(response);
 
     if (response.ok) {
-      return { success: true, user: result };
+      return { success: true, user: result.user };
     }
 
     return {
