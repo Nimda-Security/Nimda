c0bbedbaff114487584fe253e2c72974967564fd
unknown <xtkww97178@gmail.com>
2026-05-15 17:18:55 +0900

    fix: MailService
    컨트롤러가 필요하지 않기에 폴더 구조를 간소화했습니다.

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/aws/SES/MailService.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/aws/SES/MailService.java
@@ -1,9 +1,10 @@
-package com.nimda.cite.aws.SES.Service;
+package com.nimda.cite.aws.SES;
 
 import com.nimda.cite.common.util.RandomModule;
 import com.nimda.cite.common.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -19,26 +20,30 @@ public class MailService {
     private final RedisUtil redisUtil;
     private final String AUTH_PREFIX = "AUTH_CODE:";
 
-    public void checkEmail(String to) {
-        SimpleMailMessage message = new SimpleMailMessage();
+    public void sendEmail(SimpleMailMessage message) {
+        try {
+            javaMailSender.send(message);
+            log.info("메일 전송이 완료되었습니다.");
+        }catch(MailException e) {
+            log.info("메일 전송 시 오류가 발생했습니다." + e);
+        }
+    }
 
+    public SimpleMailMessage sendAuthCode(String email) {
+        SimpleMailMessage message = new SimpleMailMessage();
         String code = RandomModule.GenerateRandomStr(10,true);
         // 발신자 주소는 반드시 SES에서 인증(Verified)받은 이메일이어야 합니다.
         message.setFrom("xtkww97178@gmail.com");
-        message.setTo(to);
+        message.setTo(email);
         message.setSubject("[Nimda] 회원가입 인증번호 안내");
         message.setText("인증번호는 [" + code + "] 입니다. 5분 이내에 입력해 주세요.");
 
-        redisUtil.setDataWithExpiration("AUTH_CODE:" + to, code, 300L);
+        redisUtil.setDataWithExpiration("AUTH_CODE:" + email, code, 300L);
 
-        try {
-            javaMailSender.send(message);
-            log.info("메일 발송 성공: {}", to);
-        } catch (Exception e) {
-            log.error("메일 발송 실패: {}", e.getMessage());
-            throw new RuntimeException("이메일 발송 중 오류가 발생했습니다.");
-        }
+        sendEmail(message);
+        return message;
     }
+
 
     // code는 사용자가 입력한 번호
     public boolean verifyCode(String email, String code) {
