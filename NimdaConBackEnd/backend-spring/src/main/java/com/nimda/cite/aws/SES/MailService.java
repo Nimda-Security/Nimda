0d8369d1437f728cfd9f830b9407eb26a6cfe549
unknown <xtkww97178@gmail.com>
2026-05-18 11:28:10 +0900

    fix: 메일 전송 시 html 형식으로 전송

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/aws/SES/MailService.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/aws/SES/MailService.java
@@ -2,14 +2,18 @@ package com.nimda.cite.aws.SES;
 
 import com.nimda.cite.common.util.RandomModule;
 import com.nimda.cite.common.util.RedisUtil;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
-import java.util.Random;
 
 @Slf4j
 @Service
@@ -18,9 +22,10 @@ public class MailService {
 
     private final JavaMailSender javaMailSender;
     private final RedisUtil redisUtil;
+    private final SpringTemplateEngine templateEngine;
     private final String AUTH_PREFIX = "AUTH_CODE:";
 
-    public void sendEmail(SimpleMailMessage message) {
+    public void sendEmail(MimeMessage message) {
         try {
             javaMailSender.send(message);
             log.info("메일 전송이 완료되었습니다.");
@@ -29,18 +34,34 @@ public class MailService {
         }
     }
 
-    public SimpleMailMessage sendAuthCode(String email) {
-        SimpleMailMessage message = new SimpleMailMessage();
+    public MimeMessage sendAuthCode(String email) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+
         String code = RandomModule.GenerateRandomStr(10,true);
         // 발신자 주소는 반드시 SES에서 인증(Verified)받은 이메일이어야 합니다.
-        message.setFrom("xtkww97178@gmail.com");
-        message.setTo(email);
-        message.setSubject("[Nimda] 회원가입 인증번호 안내");
-        message.setText("인증번호는 [" + code + "] 입니다. 5분 이내에 입력해 주세요.");
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setFrom("xtkww97178@gmail.com");
+            helper.setTo(email);
+            helper.setSubject("[Nimda] 회원가입 인증번호 안내");
 
-        redisUtil.setDataWithExpiration("AUTH_CODE:" + email, code, 300L);
+            // 인증 코드 변수로 넣기
+            Context context = new Context();
+            context.setVariable("authCode", code);
 
-        sendEmail(message);
+            // 3. templates/auth-mail.html 파일을 읽어와 HTML 문자열로 변환
+            String htmlContent = templateEngine.process("password-change-mail", context);
+
+            // 4. 메일 본문 설정 (두 번째 인자를 true로 해야 HTML로 렌더링됨)
+            helper.setText(htmlContent, true);
+
+            // 인증 코드 Redis에 저장
+            redisUtil.setDataWithExpiration("AUTH_CODE:" + email, code, 300L);
+
+            sendEmail(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("HTML 메일 생성 및 발송 실패", e);
+        }
         return message;
     }
 
