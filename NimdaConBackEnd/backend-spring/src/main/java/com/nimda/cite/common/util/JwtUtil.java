ac5c49bb4ab1b21406a8c9cebea10be24f1e8783
unknown <xtkww97178@gmail.com>
2026-05-18 18:26:09 +0900

    build: fix key

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/common/util/JwtUtil.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/common/util/JwtUtil.java
@@ -83,7 +83,7 @@ public class JwtUtil {
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .signWith(this.key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
