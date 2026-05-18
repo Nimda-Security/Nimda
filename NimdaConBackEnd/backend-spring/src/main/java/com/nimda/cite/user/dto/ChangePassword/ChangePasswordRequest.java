8b2af8f5121d7898ad9be18666161e48e15d23e9
unknown <xtkww97178@gmail.com>
2026-05-18 12:50:33 +0900

    feat: 비밀번호 재설정 시 사용 DTO 추가

---
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/user/dto/ChangePassword/ChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package com.nimda.cite.user.dto.ChangePassword;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ChangePasswordRequest {
+    private String password;
+}
