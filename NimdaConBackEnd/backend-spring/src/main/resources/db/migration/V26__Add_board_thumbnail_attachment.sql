4934de90cecec3f8d502d237d82848e7d43e47aa
unknown <xtkww97178@gmail.com>
2026-05-25 18:28:44 +0900

    fix: flyway 쿼리 수정 및 nginx 설정 수정

--- NimdaConBackEnd/backend-spring/src/main/resources/db/migration/V26__Add_board_thumbnail_attachment.sql
+++ NimdaConBackEnd/backend-spring/src/main/resources/db/migration/V26__Add_board_thumbnail_attachment.sql
@@ -2,4 +2,6 @@ ALTER TABLE board
     ADD COLUMN thumbnail_attachment_id BIGINT NULL;
 
 ALTER TABLE category
-    ADD COLUMN shopEnabled BOOLEAN DEFAULT false;
+    ADD COLUMN shop_enabled BOOLEAN DEFAULT false;
+
+ALTER TABLE board ADD COLUMN item_price INT DEFAULT 0 NOT NULL;
