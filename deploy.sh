79b27177b4ffaf64a7c9e624a8cc804e3f091455
unknown <xtkww97178@gmail.com>
2026-05-25 18:35:27 +0900

    fix: 무중단 배포 명령어 수정

--- deploy.sh
+++ deploy.sh
@@ -8,16 +8,17 @@ if [ ! -f "./nginx/conf.d/active-backend.inc" ]; then
 fi
 
 # 2. 현재 Nginx 뒷단에서 구동 중인 서비스가 'blue'인지 'green'인지 헬스체크로 알아냅니다.
-ACTIVE_COLOR=$(curl -s http://localhost/api/actuator/health | grep -q "backend-blue" && echo "blue" || echo "green")
-
-# 새로운 버전이 누구힌지 확인
-if [ "$ACTIVE_COLOR" == "blue" ]; then
+if grep -q "backend-blue" "./nginx/conf.d/active-backend.inc"; then
+    ACTIVE_COLOR="blue"
     TARGET_COLOR="green"
     TARGET_PORT=8082
 else
+    ACTIVE_COLOR="green"
     TARGET_COLOR="blue"
     TARGET_PORT=8081
 fi
+
+echo "현재 가동 중인 서비스: $ACTIVE_COLOR ➡️ 새로 배포할 타겟: $TARGET_COLOR"
 
 # 4. 새로 배포할 색상의 최신 이미지를 도커허브에서 땡겨옵니다.
 docker compose pull backend-$TARGET_COLOR
@@ -25,11 +26,13 @@ docker compose pull backend-$TARGET_COLOR
 # 새로운 버전 컨테이너 실행
 docker compose up -d backend-$TARGET_COLOR
 
+echo "⏳ 새 버전($TARGET_COLOR) 부팅 및 헬스체크 대기 중..."
 # 새 버전 정상 작동 확인
 for retry in {1..24}
 do
-    status_code=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:$TARGET_PORT/api/actuator/health)
-    if [ "$status_code" -eq 200 ]; then
+    status_code=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:$TARGET_PORT/error)
+    if [ "$status_code" -ne 000]; then
+        echo "✅ $TARGET_COLOR 서버가 정상 응답합니다. (Status: $status_code)"
         break
     fi
     sleep 5
@@ -40,5 +43,7 @@ echo "server backend-$TARGET_COLOR:8080;" > ./nginx/conf.d/active-backend.inc
 
 # Nginx 프록시 서버 변경
 docker exec nimda-nginx nginx -s reload
+echo "🔄 Nginx 트래픽을 backend-$TARGET_COLOR 로 전환했습니다."
 # 기존 서버 중단
 docker compose stop backend-$ACTIVE_COLOR
+echo "🧹 구버전 서버(backend-$ACTIVE_COLOR)를 안전하게 중단했습니다."
