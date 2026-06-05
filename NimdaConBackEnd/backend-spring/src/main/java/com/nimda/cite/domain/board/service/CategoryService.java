c70f2b1504c6d610feb43a65dce8ef02a1faea99
unknown <xtkww97178@gmail.com>
2026-06-05 14:44:10 +0900

    feat: categoryService
    상점 기능 활성화 기능 추가

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/domain/board/service/CategoryService.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/domain/board/service/CategoryService.java
@@ -8,8 +8,10 @@ import com.nimda.cite.domain.board.repository.BoardRepository;
 import com.nimda.cite.domain.board.repository.CategoryRepository;
 import com.nimda.cite.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashSet;
 import java.util.List;
@@ -242,6 +244,15 @@ public class CategoryService {
                 categoryRepository.save(category);
             });
         }
+    }
+
+    @Transactional
+    public Category setShopEnabled(Long id) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow( () ->new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        category.setShopEnabled(true);
+        return category;
     }
 
     /**
