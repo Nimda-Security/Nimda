6ff65e9e042d728679e9625d1ae4806b09c0f5e3
unknown <xtkww97178@gmail.com>
2026-07-07 14:45:39 +0900

    chore:#120-컴포넌트 스캔 범위 수정

--- NimdaConBackEnd/backend-spring/src/main/java/judgeServer/domain/problem/repository/ProblemRepository.java
+++ NimdaConBackEnd/backend-spring/src/main/java/judgeServer/domain/problem/repository/ProblemRepository.java
@@ -4,7 +4,9 @@ import judgeServer.domain.problem.entity.Problem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
     Problem findByTitle(String title);
     Problem findByCode(String code);
