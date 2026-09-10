e441c5c0c2de2bc6d788d9dd44a78d99b9c51ca6
dohyeon <xtkww97178@gmail.com>
2026-09-10 14:03:48 +0900

    #143/chore(Application): redis store를 repo로 간주하지 않게 처리

--- NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/NimdaCiteApplication.java
+++ NimdaConBackEnd/backend-spring/src/main/java/com/nimda/cite/NimdaCiteApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaAuditing
 @EnableAsync // 알림 비동기 방식으로 처리
 @SpringBootApplication
+@SpringBootApplication(exclude = RedisRepositoriesAutoConfiguration.class)
 @ComponentScan(basePackages = {"com.nimda.cite", "judgeServer", "redis"})
 @EnableJpaRepositories(basePackages = {"com.nimda.cite", "judgeServer"})
 @EntityScan(basePackages = {"com.nimda.cite", "judgeServer"})
